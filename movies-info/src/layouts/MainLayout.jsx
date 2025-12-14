4354cfe3b610b84da877416741b1808374ec3005
23120394 <tuananhnguyen147123@gmail.com>
2025-12-15 00:04:11 +0700

    Tao MainLayout de cau hinh Header, NavBar va Footer

---
+++ movies-info/src/layouts/MainLayout.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import NavBar from "../components/NavBar";
+
+const MainLayout = () => {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-slate-950 transition-colors">
+      <Header />
+      <NavBar />
+
+      <main className="flex-1 container mx-auto px-4 py-8 max-w-[1200px]">
+        <Outlet />
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
