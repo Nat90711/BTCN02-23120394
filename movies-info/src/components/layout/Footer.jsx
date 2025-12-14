3028e9678d0f1da49a05b047fbd79f399cde076f
23120394 <tuananhnguyen147123@gmail.com>
2025-12-14 23:37:32 +0700

    Tao giao dien cho Footer

---
+++ movies-info/src/components/layout/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-slate-100 py-6 mt-auto border-t">
+      <div className="container mx-auto text-center text-slate-600 max-w-[1200px]">
+        <p className="font-medium">© 2025 Movies Info App</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
